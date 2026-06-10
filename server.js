4c0e09125a641c9d9b10c7f0950fed3c783b040a
shedrack <idokoshedrack52@gmail.com>
2026-06-10 13:45:16 +0100

    Initial project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname,"public/index.html"));
+});
+
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public/about.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Running on ${PORT}`);
+});
